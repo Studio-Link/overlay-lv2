fb71dcb653f31744f76088300e9d772cc051c870
Sebastian Reimers <info@miete-admin.de>
2015-11-22 13:26:26 +0100

    fix paths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../../baresip/libbaresip.a \
-    ../../re/libre.a ../../rem/librem.a ../../opus/libopus.a -I.
+gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
+    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I.
 
 ldd studio-link.so
