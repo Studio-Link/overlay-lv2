710f36059f7e7ee0bebb55e4c800805560981b7b
Sebastian Reimers <info@miete-admin.de>
2015-11-24 21:54:17 +0100

    add ssl and crypto

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include
+    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include -lssl -lcrypto
 
 ldd studio-link.so
