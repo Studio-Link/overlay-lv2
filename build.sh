dcd206cf9efd5a24d8eabfab9100616cd0914218
Sebastian Reimers <info@miete-admin.de>
2015-11-25 14:53:07 +0100

    fix openssl static

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
+
 gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include -lssl -lcrypto
+    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
+    ../openssl/libssl.a ../openssl/libcrypt.a
 
 ldd studio-link.so
