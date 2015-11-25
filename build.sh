c9d73c728eabbc364610271d9b646e2451f97f98
Sebastian Reimers <info@miete-admin.de>
2015-11-25 22:05:16 +0100

    add some link dependencies

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
     ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
-    ../openssl/libssl.a ../openssl/libcrypto.a
+    ../openssl/libssl.a ../openssl/libcrypto.a -lm -lpthread -lz -ldl -lresolv
 
 ldd studio-link.so
