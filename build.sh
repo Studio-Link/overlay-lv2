7ea4b779498119df171d603615da2248e3c0b509
Sebastian Reimers <info@miete-admin.de>
2015-11-25 15:19:03 +0100

    fix wording

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
     ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
-    ../openssl/libssl.a ../openssl/libcrypt.a
+    ../openssl/libssl.a ../openssl/libcrypto.a
 
 ldd studio-link.so
