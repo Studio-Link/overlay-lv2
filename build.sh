425197cb3b4b406ae43dd662ba7e299298c1bf93
Sebastian Reimers <hallo@studio-link.de>
2018-10-05 15:01:38 +0200

    fix openssl

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
+    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include -L ../openssl \
      -lm -lpthread -lz -ldl -lresolv -lcrypt -lssl
 
 ldd studio-link.so
