8a54d56582262182b4b87d589a72c427a2718b84
Sebastian Reimers <hallo@studio-link.de>
2019-02-23 21:28:34 +0100

    make openssl static and hide symbols - https://gitlab.com/studio.link/app/issues/8

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
-gcc -g -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include -L ../openssl \
-     -lm -lpthread -lz -ldl -lresolv -lcrypt -lssl
+gcc -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
+    ../re/libre.a ../rem/librem.a ../opus/libopus.a \
+    ../openssl/libssl.a ../openssl/libcrypto.a -I../my_include \
+     -lm -lpthread -lz -ldl -lresolv \
+     -fvisibility=hidden -fdata-sections -ffunction-sections \
+     -Wl,--gc-sections -Wl,-O1 -Wl,--as-needed -Wl,--strip-all
 
 ldd studio-link.so
