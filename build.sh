43ced44bfc96a50c37833cfe3c776c8cf73eb4d8
Sebastian Reimers <hallo@studio-link.de>
2020-01-13 11:17:31 +0100

    strip symbols

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 gcc -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
     ../re/libre.a ../rem/librem.a ../opus/libopus.a \
     ../openssl/libssl.a ../openssl/libcrypto.a -I../my_include \
-     -lm -lpthread -lz -ldl -lresolv \
-     -fvisibility=hidden -fdata-sections -ffunction-sections \
-     -Wl,--gc-sections -Wl,-O1 -Wl,--as-needed -Wl,--strip-all
+     -lm -lpthread -lz -ldl -lresolv
 
 ldd studio-link.so
+strip studio-link.so
