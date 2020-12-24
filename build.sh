794cea354106e7f691e79ee3815bb341855b1563
Sebastian Reimers <hallo@studio-link.de>
2020-12-24 10:06:53 +0100

    Update 3rdparty

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 gcc -Wall -shared -o studio-link.so -fPIC studio-link.c ../baresip/libbaresip.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a \
-    ../openssl/libssl.a ../openssl/libcrypto.a -I../my_include \
+    ../re/libre.a ../rem/librem.a ../3rdparty/lib/libopus.a \
+    ../3rdparty/lib/libssl.a ../3rdparty/lib/libcrypto.a -I../my_include \
      -lm -lpthread -lz -ldl -lresolv
 
 ldd studio-link.so
